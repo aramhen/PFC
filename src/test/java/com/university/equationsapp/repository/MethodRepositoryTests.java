773d21d923942ca455802a4563daaf906f635aa6
ElBurt <jabirto@gmail.com>
2014-10-21 13:41:11 +0200

    Added the form to make tests of repositories only with JUNIT, now spring-test it's not necessary One class use spring-test and another one only JUNIT

--- src/test/java/com/university/equationsapp/repository/MethodRepositoryTests.java
+++ src/test/java/com/university/equationsapp/repository/MethodRepositoryTests.java
@@ -1,45 +1,70 @@
 package com.university.equationsapp.repository;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.university.equationsapp.domain.Method;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, TransactionalTestExecutionListener.class })
-@Transactional
-@ContextConfiguration(locations = { "classpath:test-context.xml" })
+/**
+ * Test of MethodRepository using JUNIT
+ * @author Burt
+ *
+ */
 public class MethodRepositoryTests {
 
+	private ApplicationContext context;
 	@Autowired
 	private MethodRepository methodRepository;
 
+	/**
+	 * Method name to test
+	 */
 	private final static String METHOD_NAME = "Gauss";
 
+	/**
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("Entrando");
+		context = new ClassPathXmlApplicationContext("classpath:test-context.xml");
+		methodRepository = (MethodRepository) context.getBean("methodRepository");
+		String[] tmp = context.getBeanDefinitionNames();
+		int sizea = tmp.length;
+		System.out.println("Tamaño: " + sizea);
+		for (int i = 0; i < sizea; i++) {
+			System.out.println(tmp[i]);
+		}
+	}
+
 	//ARH TODO limpiar system.outs
+	/**
+	 * 
+	 */
 	@Test
 	public void testFindByIdMethods() {
 		System.out.println("testFindByIdMethods");
 		Method method = methodRepository.findByIdMethods(1);
-		Assert.assertNotNull(method);
+		assertNotNull(method);
 		System.out.println("First Method: " + method.getMethodName());
 	}
 
+	/**
+	 * 
+	 */
 	@Test
 	public void testfindByMethodName() {
 		System.out.println("testfindByMethodName");
 		Method method = methodRepository.findByMethodName(METHOD_NAME);
-		Assert.assertNotNull(method);
+		assertNotNull(method);
 		System.out.println("Gauss Method Id: " + method.getIdMethods());
-		Assert.assertEquals(METHOD_NAME, method.getMethodName());
+		assertEquals(METHOD_NAME, method.getMethodName());
 	}
 
 }
---
+++ src/test/java/com/university/equationsapp/repository/MethodRepositoryTestsSpring.java
@@ -0,0 +1,59 @@
+package com.university.equationsapp.repository;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.university.equationsapp.domain.Method;
+
+/**
+ * Test of MethodRepository using spring-test
+ * @author Burt
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, TransactionalTestExecutionListener.class })
+@Transactional
+@ContextConfiguration(locations = { "classpath:test-context.xml" })
+public class MethodRepositoryTestsSpring {
+
+	@Autowired
+	private MethodRepository methodRepository;
+
+	/**
+	 * Method name to test
+	 */
+	private final static String METHOD_NAME = "Gauss";
+
+	//ARH TODO limpiar system.outs
+	/**
+	 * 
+	 */
+	@Test
+	public void testFindByIdMethods() {
+		System.out.println("testFindByIdMethods");
+		Method method = methodRepository.findByIdMethods(1);
+		Assert.assertNotNull(method);
+		System.out.println("First Method: " + method.getMethodName());
+	}
+
+	/**
+	 * 
+	 */
+	@Test
+	public void testfindByMethodName() {
+		System.out.println("testfindByMethodName");
+		Method method = methodRepository.findByMethodName(METHOD_NAME);
+		Assert.assertNotNull(method);
+		System.out.println("Gauss Method Id: " + method.getIdMethods());
+		Assert.assertEquals(METHOD_NAME, method.getMethodName());
+	}
+
+}
