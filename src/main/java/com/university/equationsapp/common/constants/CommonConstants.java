ac154293da5b0a09cb55c3ca22e6db3bf2f3af33
ElBurt <jabirto@gmail.com>
2014-11-26 20:05:55 +0100

    Created Constants

---
+++ src/main/java/com/university/equationsapp/common/constants/CommonConstants.java
@@ -0,0 +1,8 @@
+package com.university.equationsapp.common.constants;
+
+public final class CommonConstants {
+	
+	//Constants to refer to the number of variables of the problems
+	public static final String SEPARATOR = "|";
+
+}
---
+++ src/main/java/com/university/equationsapp/web/constants/WebConstants.java
@@ -0,0 +1,10 @@
+package com.university.equationsapp.web.constants;
+
+public final class WebConstants {
+	
+	//Constants to refer to the number of variables of the problems
+	public static final int NUM_VARIABLES_MIN = 2;
+	public static final int NUM_VARIABLES_MAX = 3;
+	public static final int[] NUM_VARIABLES = {2, 3};
+
+}
